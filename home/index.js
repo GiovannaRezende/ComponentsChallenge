690b9334c3d5804b29c9aaa4577ca69f7c8e18e1
Giovanna <giovannadossantos2004@gmai.com>
2021-10-05 18:23:28 -0300

    okkkkkkkkk

--- home/index.js
+++ home/index.js
@@ -1,3 +1,41 @@
+import { Container } from "./styled"
+import { useState } from "react"
+
+export default function Home() {
+
+    const [produtos, setProdutos] = useState([]);
+
+    function listar() {
+        const r = [
+            {
+                id: 10001,
+                imagem: "",
+                titulo: "",
+                preço: "",
+                descrição: "",
+                especificações: ""
+            },
+            {
+                id: 10001,
+                imagem: "",
+                titulo: "",
+                preço: "",
+                descrição: "",
+                especificações: ""
+            },
+            {
+                id: 10001,
+                imagem: "",
+                titulo: "",
+                preço: "",
+                descrição: "",
+                especificações: ""
+            }
+        ]
+
+        setProdutos(r);
+    }
+
 return (
     <Container>
         <div clas="titulo">Seja bem-vindo ao supermercado AllMarket!</div>
@@ -9,3 +47,5 @@ return (
         </div>
     </Container>
 )
+
+}
